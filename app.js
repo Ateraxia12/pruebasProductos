0021930dd16b61123021e6bc2f2ba28598d746d5
Su nombre <SENA@ARMAPRCCIFOD607.FormacionSena.RED>
2024-09-05 08:45:56 -0500

    pruebas-0.8

--- app.js
+++ app.js
@@ -13,3 +13,41 @@ document.getElementById('filterInput').addEventListener('keyup', function() {
         }
     });
 });
+
+// buscador-2
+document.getElementById('filterInput-2').addEventListener('keyup', function() {
+    const filterValue = this.value.toLowerCase();
+    const items = document.querySelectorAll('#itemsList li');
+    
+    items.forEach(function(item) {
+        const itemText = item.textContent.toLowerCase();
+        if (itemText.includes(filterValue)) {
+            item.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden');
+        }
+    });
+});
+
+// --------------
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+
+    menuToggle.addEventListener('click', (event) => {
+        navLinks.classList.toggle('active');
+        event.stopPropagation(); // Evita que el clic en el botón de hamburguesa cierre el menú inmediatamente
+    });
+
+    // Cierra el menú cuando se hace clic fuera de él
+    document.addEventListener('click', (event) => {
+        if (!navLinks.contains(event.target) && !menuToggle.contains(event.target)) {
+            navLinks.classList.remove('active');
+        }
+    });
+
+    // Evita el cierre del menú si se hace clic dentro del menú
+    navLinks.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+});
